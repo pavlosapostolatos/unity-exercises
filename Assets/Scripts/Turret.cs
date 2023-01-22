16c3dea8cdc936805ff1d79f46d34c1fb54c9154
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2023-01-22 15:03:02 +0200

    set up guns

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,11 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using Color = UnityEngine.Color;
 
 public class Turret : MonoBehaviour
 {
     public Transform turret;
+
+    public Transform gun1;
+    
+    public Transform gun2;
+
+    public float height; //0.5
+
+    public float distance; //0,.8 for my cube
+
+    public float size1;
+    
+    public float size2;
+  
+    public float size3;
 
     
     Vector3[] corners = new Vector3[]{
@@ -56,6 +72,14 @@ public class Turret : MonoBehaviour
                 // Gizmos.DrawSphere(cornerToWorld,0.3f);
 
             }
+
+            gun1.position = turret.position + yAxis * height;
+            gun1.position += xAxis * distance;
+            gun1.localScale = new Vector3(size1, size2, size3);//should have 3 sizes
+            
+            gun2.position = turret.position + yAxis * height;
+            gun2.position -= xAxis * distance;
+            gun2.localScale = new Vector3(size1, size2, size3);//should have 3
         }
     }
 }
