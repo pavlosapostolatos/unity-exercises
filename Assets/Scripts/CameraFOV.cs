543a28688ccf99b9393cc400cf664f7c9a114f79
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2023-01-27 20:56:07 +0200

    take radius into account

--- Assets/Scripts/CameraFOV.cs
+++ Assets/Scripts/CameraFOV.cs
@@ -7,6 +7,8 @@ public class CameraFOV : MonoBehaviour
 {
     public Transform[] points;
 
+    [Range(0f, 5f)] public float radius = 0.5f;
+
     public void OnDrawGizmos()
     {
         float maxAngle = 0f;
@@ -14,19 +16,25 @@ public class CameraFOV : MonoBehaviour
 
         foreach (Transform point in points)
         {
+            Gizmos.DrawWireSphere(point.position,radius);
+            
             Vector2 relPos = point.position - cam.transform.position;
 
-            float opposite = relPos.y;
+            float opposite = Mathf.Abs(relPos.y);
             float adjacent = relPos.x;
 
             float angle = Mathf.Atan2(opposite, adjacent) * Mathf.Rad2Deg;
 
-            maxAngle = Mathf.Max(Mathf.Abs(angle), maxAngle);
+            float extraAngleWithRadius = Mathf.Asin(radius / relPos.magnitude) * Mathf.Rad2Deg;
+            angle += extraAngleWithRadius;
             
+            maxAngle = Mathf.Max(Mathf.Abs(angle), maxAngle);
+
             cam.fieldOfView = maxAngle;
 
             //draw an infinite line based on an angle
         }
+        
         Gizmos.color = Color.red;
         Vector3 dir = Quaternion.AngleAxis(maxAngle, Vector3.forward) * Vector3.right;
         Gizmos.DrawLine(cam.transform.position, cam.transform.position + dir * 100);
