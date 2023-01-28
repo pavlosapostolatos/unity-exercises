69c7881b4cba23329e97b6bb0e78f284156bd5cd
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2023-01-28 20:10:56 +0200

    backup

--- Assets/Scripts/CameraFOV.cs
+++ Assets/Scripts/CameraFOV.cs
@@ -11,34 +11,47 @@ public class CameraFOV : MonoBehaviour
 
     public void OnDrawGizmos()
     {
+        Debug.Log(Mathf.Atan2(0.0f, 1) * Mathf.Rad2Deg);
         float maxAngle = 0f;
         Camera cam = GetComponent<Camera>();
-
         foreach (Transform point in points)
         {
-            Gizmos.DrawWireSphere(point.position,radius);
-            
-            Vector2 relPos = cam.transform.InverseTransformPoint(point.position);
-
-            float opposite = Mathf.Abs(relPos.y);
-            float adjacent = relPos.x;
-
-            float angle = Mathf.Atan2(opposite, adjacent) * Mathf.Rad2Deg;
-
-            float extraAngleWithRadius = Mathf.Asin(radius / relPos.magnitude) * Mathf.Rad2Deg;
-            angle += extraAngleWithRadius;
-            
-            maxAngle = Mathf.Max(Mathf.Abs(angle), maxAngle);
-
-            cam.fieldOfView = maxAngle;
-
-            //draw an infinite line based on an angle
+            Gizmos.DrawWireSphere(point.position, radius);
+            Vector3 relPos = cam.transform.InverseTransformPoint(point.position);
+            maxAngle = MaxAngleIn2dSpace(cam, maxAngle,relPos);
+            maxAngle = MaxAngleIn2dSpace(cam, maxAngle,new Vector2(relPos.z,relPos.y));
         }
-        
+
         Gizmos.color = Color.red;
         Vector3 dir = Quaternion.AngleAxis(maxAngle, Vector3.forward) * Vector3.right;
         Gizmos.DrawLine(cam.transform.position, cam.transform.position + dir * 100);
         dir = Quaternion.AngleAxis(-maxAngle, Vector3.forward) * Vector3.right;
         Gizmos.DrawLine(cam.transform.position, cam.transform.position + dir * 100);
+        // always positive of the right vector and moving along the up vector
+
+        dir = Quaternion.AngleAxis(maxAngle, Vector3.up) * Vector3.right;
+        Gizmos.DrawLine(cam.transform.position, cam.transform.position + dir * 100);
+        dir = Quaternion.AngleAxis(-maxAngle, Vector3.up) * Vector3.right;
+        Gizmos.DrawLine(cam.transform.position, cam.transform.position + dir * 100);
+        // always positive of the right vector and moving along the forward vector
+    }
+
+    private float MaxAngleIn2dSpace(Camera cam, float maxAngle ,Vector2 point)
+    {
+        float opposite = Mathf.Abs(point.y);
+        float adjacent = point.x;
+
+        float angle = Mathf.Atan2(opposite, adjacent) * Mathf.Rad2Deg;
+
+        float extraAngleWithRadius = Mathf.Asin(radius / point.magnitude) * Mathf.Rad2Deg;
+        angle += extraAngleWithRadius;
+
+        maxAngle = Mathf.Max(Mathf.Abs(angle), maxAngle);
+
+        cam.fieldOfView = maxAngle;
+
+        //draw an infinite line based on an angle
+
+        return maxAngle;
     }
 }
