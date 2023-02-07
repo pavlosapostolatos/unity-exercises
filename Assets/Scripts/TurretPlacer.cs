a6d8d5a47b3125430f0b224b5b607fd0ec3e5516
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2023-02-07 09:01:52 +0200

    implement mouse look to turret placement

--- Assets/Scripts/TurretPlacer.cs
+++ Assets/Scripts/TurretPlacer.cs
@@ -7,18 +7,40 @@ public class TurretPlacer : MonoBehaviour {
 
 	public Transform turret;
 
-	void OnDrawGizmos() {
-		if( turret == null )
-			return;
+	float pitchDeg;
+	float yawDeg;
+	
+	public bool invertYAxis = false;
+
+	void Awake() {
+		Vector3 startEuler = transform.eulerAngles;
+		pitchDeg = startEuler.x;
+		yawDeg = startEuler.y;
+		transform.rotation = Quaternion.Euler( pitchDeg, yawDeg, 0 );
+	}
+	
+	void Update()
+	{
 		Ray ray = new Ray( transform.position, transform.forward );
 		if( Physics.Raycast( ray, out RaycastHit hit ) ) {
 			turret.position = hit.point;
 			Vector3 yAxis = hit.normal;
 			Vector3 zAxis = Vector3.Cross( transform.right, yAxis ).normalized;
-			Gizmos.color = new Color( 1, 1, 1, 0.4f );
-			Gizmos.DrawLine( ray.origin, hit.point );
+			// Debug.color = new Color( 1, 1, 1, 0.4f );
+			Debug.DrawLine( ray.origin, hit.point,new Color( 1, 1, 1, 0.4f ) );
 			turret.rotation = Quaternion.LookRotation( zAxis, yAxis );
 		}
+		UpdateMouseLook();
+	}
+
+	void UpdateMouseLook() {
+		float xDelta = Input.GetAxis( "Mouse X" );
+		float yDelta = Input.GetAxis( "Mouse Y" );
+		Debug.Log( $"xDelta={xDelta}, yDelta={yDelta}" );
+		pitchDeg += invertYAxis ? yDelta : -yDelta;
+		pitchDeg = Mathf.Clamp( pitchDeg, -90, 90 );
+		yawDeg += xDelta;
+		transform.rotation = Quaternion.Euler( pitchDeg, yawDeg, 0 );
 	}
 
 }
