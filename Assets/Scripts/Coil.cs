922f40ab56fdabaaae761af91970e24ab5848319
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2023-02-11 15:27:14 +0200

    finish coil

--- Assets/Scripts/Coil.cs
+++ Assets/Scripts/Coil.cs
@@ -9,40 +9,39 @@ public class Coil : MonoBehaviour
     public int height = 3;
     public int radius = 3;
     public int detail = 100;
-    public Vector2 AngToDir(float dir) => new Vector2(Mathf.Cos(dir), Mathf.Sin(dir));
+    public Vector3 AngToDir(float dir) => new Vector3(Mathf.Cos(dir), 0, Mathf.Sin(dir));
 
 
     private void OnDrawGizmos()
     {
-        // Gizmos.DrawWireSphere(default, 1.0f);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(default, Vector3.up * height * spins);
         Gizmos.DrawSphere(default, 0.3f);
-        // Gizmos.DrawSphere(Vector3.Slerp(Vector3.right, Vector3.up, 0.5f), 0.3f);
-        // Gizmos.DrawSphere(Vector3.Slerp(Vector3.right, -Vector3.up, 0.5f), 0.3f);
-        // Gizmos.DrawSphere(Vector3.Slerp(Vector3.right, -Vector3.right, 0.5f), 0.3f);
-        // Debug.Log(Vector3.Slerp(Vector3.right, -Vector3.right, 0.5f));
-        Gizmos.DrawSphere(Vector3.Slerp(Vector3.right, AngToDir(179 * Mathf.Deg2Rad), 0.5f), 0.3f);
-        Gizmos.DrawSphere(Vector3.Slerp(Vector3.right, AngToDir(181 * Mathf.Deg2Rad), 0.5f), 0.3f);
 
         Vector3 firstCheckPoint = AngToDir(179 * Mathf.Deg2Rad);
         Vector3 secondCheckPoint = AngToDir(181 * Mathf.Deg2Rad);
 
         Vector3 prevPoint = Vector3.right;
-        Vector3 nextPoint = Vector3.right;
-        for (int i = 0; i < detail; i++)
+        Vector3 nextPoint;
+        for (int spin = 0; spin < spins; spin++)
         {
-            if (i < detail / 2)
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(Vector3.right * radius + Vector3.up * height * spin, 0.3f); //signify start of new spin
+            for (int i = 0; i < detail; i++)
             {
-                nextPoint = Vector3.Slerp(Vector3.right, firstCheckPoint, (2f * i) / detail);
-                Gizmos.DrawLine(prevPoint, nextPoint);
-                prevPoint = nextPoint;
-            }
-            else if (i > detail / 2) //if == 180 ignore(edgecase)
-            {
-                nextPoint = Vector3.Slerp(Vector3.right, secondCheckPoint, 1f - ((i - detail / 2f) / (detail / 2f)));
+                if (i < detail / 2)
+                    nextPoint = radius * Vector3.Slerp(Vector3.right, firstCheckPoint, (2f * i) / detail);
+                else
+                {
+                    nextPoint = radius * Vector3.Slerp(Vector3.right, secondCheckPoint,
+                        1f - ((i - detail / 2f) / (detail / 2f)));
+                }
+
+                Gizmos.color = Color.white;
+                nextPoint.y = spin * height + (float)i / detail * height;
                 Gizmos.DrawLine(prevPoint, nextPoint);
                 prevPoint = nextPoint;
             }
         }
-        // Gizmos.DrawSphere(Vector3.Slerp(Vector3.right, AngToDir(359 * Mathf.Deg2Rad), 0.5f), 0.3f);
     }
 }
