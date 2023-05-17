e67771114d6faf9ad30e911bcdbf212c6de228e9
Pavlos Apostolatos <pavlos.apostolatos@gmail.com>
2023-05-17 23:45:28 +0300

    add set up for unit tests

---
+++ Assets/Tests/EditMode/WorldLocalEditTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class WorldLocalEditTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void WorldLocalTestSimpleEdit()
+    {
+        // Use the Assert class to test conditions
+    }
+}
